b211968a95373093576f14b155a65a63b799e798
jlmodell <48035911+jlmodell@users.noreply.github.com>
2022-12-20 10:02:12 -0500

    push

--- connections/remote_connection.py
+++ connections/remote_connection.py
@@ -5,3 +5,4 @@ client = MongoClient(os.environ["ATLAS_MONGODB_URI"])
 db = client.get_database("busserebatetraces")
 
 rebate_tracings_collection = db.get_collection("tracings")
+sales_tracings_collection = db.get_collection("sales_tracings")
